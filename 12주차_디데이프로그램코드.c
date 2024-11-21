f4e731101fc39e1942b92aa0e44050d827f0f88d
yvvoo <ywjung0987@gmail.com>
2024-11-21 20:47:12 +0900

    Update 12주차_디데이프로그램코드.c

--- 12주차_디데이프로그램코드.c
+++ 12주차_디데이프로그램코드.c
@@ -32,3 +32,34 @@ int main(int argc, char* argv[])
 {
     week2(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]));
 }
+
+
+
+
+
+// 실습코드도 적었습니다.
+#include <stdio.h>
+#include <time.h>
+#include "report.h"
+
+int main(int argc, char* argv[])
+{
+    time_t current;
+    struct tm* timeinfo;
+    
+    int year, month, day;
+    char* WEEK[] = {"sun", "mon", "tue", "wed", "thu", "fri", "sat"};
+    
+    printf("Enter YYYY MM DD : ");
+    scanf("%d %d %d", &year, &month, &day);
+    
+    current = time(NULL);
+    timeinfo = localtime(&current);
+    
+    timeinfo->tm_year = year-1900;
+    timeinfo->tm_mon = month - 1;
+    timeinfo->tm_mday = day;
+    
+    mktime(timeinfo);
+    
+    printf("%d %d %d -> %s\n", year, month, day, WEEK[timeinfo->tm_wday]);
